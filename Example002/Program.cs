3d97148afb7fa935cf0ae857e5223a673f12be82
Barys Smaliak <smboris@yandex.ru>
2023-04-07 12:06:16 +0300

    Example002 added

---
+++ Example002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+int ReadNum (string arg)
+{
+    int number;
+    Console.WriteLine($"Enter {arg}");
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("It,s not a number");
+    }
+    return number;
+}
+
+void SumDigit(int a)
+{
+    int result = 0;
+    while (a >0)
+    {
+        result = result + a % 10;
+        a = a / 10;
+    }
+    Console.WriteLine(
+    "The sum of the digit of this number is " + result);
+}
+
+int sum = ReadNum ("number");
+SumDigit (sum);
