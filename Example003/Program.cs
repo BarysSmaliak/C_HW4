e1f062a1051f70434c309a85f32078a7a29ec87b
Barys Smaliak <smboris@yandex.ru>
2023-04-07 12:54:25 +0300

    example003 added

---
+++ Example003/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, 
+// которая задаёт массив из 8 элементов и выводит их на экран
+
+int ReadNum (string arg)
+{
+    int number;
+    Console.WriteLine($"Enter {arg}");
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("It,s not a number");
+    }
+    return number;
+}
+
+int ArrayLength = ReadNum ("array length:");
+int min = ReadNum ("minimum element size:");
+int max = ReadNum ("maximum element size:");
+
+void CreateArroy (int length, int min, int max)
+{
+    int [] array = new int [length];
+    Console.Write ("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new  Random (). Next (min, max+1);
+        Console.Write (array[i] + "; ");
+    }
+    Console.Write ("]");
+}
+
+CreateArroy (ArrayLength, min, max);
