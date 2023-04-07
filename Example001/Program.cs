4a29a020ed2d67863a45c21ba7f32a114fe3130c
Barys Smaliak <smboris@yandex.ru>
2023-04-07 11:46:11 +0300

    Example001 changed

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,3 +1,25 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+int ReadNum (string arg)
+{
+    int number;
+    Console.WriteLine($"Input {arg}:");
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine($"It,s not a {arg}");
+    }
+    return number;
+}
+
+int num = ReadNum("number");
+int power = ReadNum("power of number");
+
+double PowerNum (int a, int b)
+{
+    double result = Math.Pow(a, b);
+    return result;
+}
+
+double result = PowerNum(num, power);
+Console.WriteLine("Result is " + result);
